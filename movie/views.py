526ed076154e5279d0d8be4c3eb995c3c8c83fb7
Ali <alinazari858585@gmail.com>
2025-08-31 13:46:23 +0330

    feat: Implement Movie and Genre viewsets with search functionality

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import filters, viewsets
 
-# Create your views here.
+from .models import Genre, Movie
+from .serializers import GenreSerializer, MovieSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all().prefetch_related("genres")
+    serializer_class = MovieSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "genres__name"]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
