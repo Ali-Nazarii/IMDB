e602dae1d940c907101960a13ad8d732c45adea5
Ali <alinazari858585@gmail.com>
2025-08-31 11:25:45 +0330

    feat: Implement RegisterView for user registration

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = RegisterSerializer
