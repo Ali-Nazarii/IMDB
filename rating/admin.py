685013ad5676925c804f6e582ee53e06b5a7bc6f
Ali <alinazari858585@gmail.com>
2025-08-31 14:36:51 +0330

    feat: Enhance admin panel for Rating model

--- rating/admin.py
+++ rating/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rating
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["user", "movie", "score", "created_at", "updated_at"]
+    list_filter = ["score", "created_at", "updated_at"]
+    search_fields = ["user__username", "movie__title", "score"]
+    readonly_fields = ["created_at", "updated_at"]
+    autocomplete_fields = ["user", "movie"]
+    ordering = ["-created_at"]
+
+    fieldsets = (
+        ("Rating Information", {"fields": ("user", "movie", "score")}),
+        (
+            "Timestamps",
+            {"fields": ("created_at", "updated_at"), "classes": ("collapse",)},
+        ),
+    )
+
+    def user_username(self, obj):
+        """Display username for better readability"""
+        return obj.user.username
+
+    user_username.short_description = "Username"
+    user_username.admin_order_field = "user__username"
+
+    def movie_title(self, obj):
+        """Display movie title for better readability"""
+        return obj.movie.title
+
+    movie_title.short_description = "Movie Title"
+    movie_title.admin_order_field = "movie__title"
+
+
+admin.site.register(Rating, RatingAdmin)
